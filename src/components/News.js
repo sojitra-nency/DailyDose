7fd0a4a36b6fb27c5d3b1327ca2d506d9f9a597e
nency <snency16@gmail.com>
2024-04-09 16:35:08 +0530

    Refactoring news component to use same function

--- src/components/News.js
+++ src/components/News.js
@@ -18,64 +18,50 @@ export class News extends Component {
         category: PropTypes.string
     }
     
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         // console.log("Hello from News Component");
         this.state = {
             articles: [],
             loading: false,
             page: 1
         }
+        document.title = `DailyDose - ${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)}`;
     }
 
-    async componentDidMount(){
-        // let url = `https://newsapi.org/v2/top-headlines?apiKey=803d58f3059445539c08249a7a68fcae&q=cricket&page=1&pageSize=${this.props.pageSize}`;   
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=803d58f3059445539c08249a7a68fcae&page=1&pageSize=${this.props.pageSize}` 
+    updateNews = async ()=>{
+        // let url = `https://newsapi.org/v2/top-headlines?apiKey=803d58f3059445539c08249a7a68fcae&q=cricket&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=803d58f3059445539c08249a7a68fcae&page=${this.state.page}&pageSize=${this.props.pageSize}`
         this.setState({loading: true})
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
         this.setState({
-            articles: parsedData.articles, 
-            totalResults: parsedData.totalResults,
+            articles: parsedData.articles,
             loading: false
         })
     }
 
+    async componentDidMount(){
+        this.setState({page: 1});
+        this.updateNews();
+    }
+
     handlePrevClick = async ()=>{
-        // let url = `https://newsapi.org/v2/top-headlines?apiKey=803d58f3059445539c08249a7a68fcae&q=cricket&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=803d58f3059445539c08249a7a68fcae&page=1&pageSize=${this.props.pageSize}` 
-        this.setState({loading: true})
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({loading: false})
-        // console.log(parsedData);
-        this.setState({
-            page: this.state.page-1,
-            articles: parsedData.articles
-        })
+        this.setState({page: this.state.page-1});
+        this.updateNews();
     }
 
     handleNextClick = async ()=>{
-        // let url = `https://newsapi.org/v2/top-headlines?apiKey=803d58f3059445539c08249a7a68fcae&q=cricket&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=803d58f3059445539c08249a7a68fcae&page=1&pageSize=${this.props.pageSize}` 
-        this.setState({loading: true})
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({loading: false})
-        // console.log(parsedData);
-        this.setState({
-            page: this.state.page+1,
-            articles: parsedData.articles
-        })
-        
+        this.setState({page: this.state.page+1});
+        this.updateNews();
     }
 
   render() {
     
     return (
       <div className="container my-3 text-center">
-        <h2 className="text-center" style={{margin: "35px 0px"}}> DailyDose - Top Headlines</h2>
+        <h2 className="text-center" style={{margin: "35px 0px"}}> DailyDose - Top  {this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} Headlines</h2>
         {this.state.loading && <Spinner/>}
         <div className="row">
         {!this.state.loading && this.state.articles.map((element)=>{
