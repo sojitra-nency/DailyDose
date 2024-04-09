030841a0e80707e5a1c56bbcd986a5b9c1c99d22
nency <snency16@gmail.com>
2024-04-09 12:10:42 +0530

    fetching data directly using API

--- src/components/News.js
+++ src/components/News.js
@@ -2,44 +2,24 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    articles = [
-          {
-            "source": {
-              "id": "espn-cric-info",
-              "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-          },
-          {
-            "source": {
-              "id": "espn-cric-info",
-              "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-          }
-        ]
-      
-
+    
     constructor(){
         super();
         console.log("Hello from News Component");
         this.state = {
-            articles: this.articles,
+            articles: [],
             loading: false
         }
     }
+
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?apiKey=803d58f3059445539c08249a7a68fcae&q=cricket";    
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({articles: parsedData.articles})
+    }
+
   render() {
     
     return (
@@ -47,8 +27,8 @@ export class News extends Component {
         <h2> DailyDose - Top Headlines</h2>
         <div className="row">
         {this.state.articles.map((element)=>{
-            return <div className="col-md-4 my-2" key={element.url}>
-                <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,95)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            return <div className="col-md-4 mx-2 my-2" key={element.url}>
+                <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,95):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
                 </div>
         })}   
         </div>    
