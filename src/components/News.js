54bbdd007ff367ee6ce685517b8e9f78563b64a6
nency <snency16@gmail.com>
2024-04-10 16:01:17 +0530

    adding top loading bar

--- src/components/News.js
+++ src/components/News.js
@@ -30,20 +30,22 @@ export class News extends Component {
     }
 
     updateNews = async ()=>{
-       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=803d58f3059445539c08249a7a68fcae&page=${this.state.page}&pageSize=${this.props.pageSize}`
+        this.props.setProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=803d58f3059445539c08249a7a68fcae&page=${this.state.page}&pageSize=${this.props.pageSize}`
         this.setState({loading: true})
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json();
-        console.log(parsedData);
+        this.props.setProgress(70);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false
         })
+        this.props.setProgress(100);
     }
 
     async componentDidMount(){
-        this.setState({page: 1});
         this.updateNews();
     }
 
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,14 +7,17 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: "0%", zIndex: "1"}}> {source} </span>
-          <img src={imageUrl?imageUrl:"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"} className="card-img-top" alt="..."/>
+          <div style = {{display: "flex", justifyContent: "flex-end"}}> 
+            <span className="position-absolute top-0 badge rounded-pill bg-danger" style={{right: "0%", zIndex: "1"}}> {source} </span>
+          </div>
+          <img src={imageUrl?imageUrl:"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
             <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()}</small></p>
             <Link to={newsUrl} className="btn btn-dark btn-sm">Read More</Link>
           </div>
+          
         </div>
       </div>
     )
