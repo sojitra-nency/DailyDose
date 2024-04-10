bb91fb1c00fc66c6813c6b89b7bb02f65d74d328
nency <snency16@gmail.com>
2024-04-10 16:50:38 +0530

    fixing issues

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,7 @@ export class NewsItem extends Component {
           <div style = {{display: "flex", justifyContent: "flex-end"}}> 
             <span className="position-absolute top-0 badge rounded-pill bg-danger" style={{right: "0%", zIndex: "1"}}> {source} </span>
           </div>
-          <img src={imageUrl?imageUrl:"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"} className="card-img-top" alt="..." />
+          <img src={imageUrl?imageUrl:"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"} className="card-img-top" alt="..." style={{height:"250px", width:"450"}}/>
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
