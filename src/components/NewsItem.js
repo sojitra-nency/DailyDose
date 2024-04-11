2176fdaf52f96524d9ce860123342d964e2dc86a
nency <snency16@gmail.com>
2024-04-11 11:35:51 +0530

    removing unnecessary comments and codes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,35 +1,45 @@
-// import React, { Component } from 'react'
-// In functional component
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-//uncomment for class component
-// export class NewsItem extends Component {
-  // In functional component
-  const NewsItem = (props) => {
-    //uncomment for class component
-  // render() {
-    // let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
-    let {title, description, imageUrl, newsUrl, author, date, source} = props; 
-    return (
-      <div>
-        <div className="card">
-          <div style = {{display: "flex", justifyContent: "flex-end"}}> 
-            <span className="position-absolute top-0 badge rounded-pill bg-danger" style={{right: "0%", zIndex: "1"}}> {source} </span>
-          </div>
-          <img src={imageUrl?imageUrl:"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"} className="card-img-top" alt="..." style={{height:"250px", width:"450"}}/>
-          <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()}</small></p>
-            <Link to={newsUrl} className="btn btn-dark btn-sm">Read More</Link>
-          </div>
-          
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div>
+      <div className="card">
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <span
+            className="position-absolute top-0 badge rounded-pill bg-danger"
+            style={{ right: "0%", zIndex: "1" }}
+          >
+            {" "}
+            {source}{" "}
+          </span>
+        </div>
+        <img
+          src={
+            imageUrl
+              ? imageUrl
+              : "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
+          }
+          className="card-img-top"
+          alt="..."
+          style={{ height: "250px", width: "450" }}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {author} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <Link to={newsUrl} className="btn btn-dark btn-sm">
+            Read More
+          </Link>
         </div>
       </div>
-    )
-    //uncomment for class component
-  // }
-}
+    </div>
+  );
+};
 
-export default NewsItem
+export default NewsItem;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,23 +1,12 @@
-// uncomment for class component
-// import React, { Component } from 'react'
+import React from "react";
+import spinner from "./spinner.gif";
 
-// In functional component
-import React from 'react' 
-import spinner from './spinner.gif';
+const Spinner = () => {
+  return (
+    <div className="text-center">
+      <img src={spinner} alt="Loading" />
+    </div>
+  );
+};
 
-//uncomment for class component
-// export class Spinner extends Component {
-  // In functional component
-  const Spinner = () => {
-    //uncomment for class component
-  // render() {
-    return (
-      <div className='text-center'>
-        <img src={spinner} alt="Loading" />
-      </div>
-    )
-    //uncomment for class component
-  // }
-}
-
-export default Spinner
+export default Spinner;
